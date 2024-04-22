b89f25247b7073d6a950bb763d42fe202bf92d36
root <root@GreenCloud.1713778544>
2024-04-22 16:01:42 +0100

    xkr

--- xkr.sh
+++ xkr.sh
@@ -1,2 +1,4 @@
-chmod +x xmrig
-./xmrig -c config-xkr.json
+#!/bin/bash
+ps -ef|grep config-xkr.json |grep -v grep|cut -c 9-16|`xargs kill -9` 
+~/longmine/xmrig -c ~/longmine/config-xkr.json -l ~/longmine/xkr.log -B
+echo "run done"
