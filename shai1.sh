eff83f171059129155b7796680c2fbf0751eeaa7
geniuszhe <geniuszhe@163.com>
2024-12-01 15:31:43 +0000

    1

--- shai1.sh
+++
@@ -1,2 +0,0 @@
-ps -ef|grep shaipot |grep -v grep|cut -c 9-16|xargs kill -9
-nohup /root/shaipot/target/release/shaipot --address sh1qts342juwawytvz2fsccr4jlvfje5hxwu706jnk  --pool wss://pool.shaicoin.fun --threads 1 >> shaipot.log &
